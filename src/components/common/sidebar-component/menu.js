bfb259af39902fa5b3fda6f2463361676b90c720
manansh <manansh.arora@gmail.com>
2022-10-06 20:06:00 +0530

    Added basic calc component & menu item

--- src/components/common/sidebar-component/menu.js
+++ src/components/common/sidebar-component/menu.js
@@ -9,5 +9,8 @@ export const MENUITEMS = [
     },
     {
         path:'/dashboard/session/', title: 'Six Step Study', icon: FileText, type: 'link', badgeType: 'primary', active: false
+    },
+    {
+        path:'', title: 'Calculators', icon: FileText, type: 'link', badgeType: 'primary', active: false
     }
 ]
